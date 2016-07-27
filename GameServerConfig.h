ea13b297460f419833beb774390e180c83af0f54
Sean Bollin <sean.bollin@dev9.com>
2016-07-26 18:49:39 -0700

    initial commit

---
+++ GameServerConfig.h
@@ -0,0 +1,3 @@
+// the configured options and settings for GameServer
+#define GameServer_VERSION_MAJOR 0
+#define GameServer_VERSION_MINOR 1
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "GameServerConfig.h"
+
+int main(int argc, char* argv[]) {
+    std::cout << "GameServer version: " << GameServer_VERSION_MAJOR << "." << GameServer_VERSION_MINOR << "\n";
+    
+    return 0;
+}
