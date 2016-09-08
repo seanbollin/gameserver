da4c61ee04c9dc231797790844c494f0e908d8b7
Sean Bollin <sean.bollin@dev9.com>
2016-09-07 18:43:00 -0700

    fix lint errors

--- client/visuals/Dot.h
+++ client/visuals/Dot.h
@@ -1,31 +1,28 @@
-#ifndef GAMESERVER_DOT_H
-#define GAMESERVER_DOT_H
+#ifndef CLIENT_VISUALS_DOT_H_
+#define CLIENT_VISUALS_DOT_H_
 
 #include <SDL_events.h>
 
 class Dot {
-public:
+ public:
   static const int DOT_WIDTH = 20;
   static const int DOT_HEIGHT = 20;
 
   static const int DOT_VEL = 10;
 
-  void handleEvent(SDL_Event& e);
+  void handleEvent(const SDL_Event& e);
   void move();
   void render();
 
-private:
+  int getVelX() const;
+  int getVelY() const;
+
+ private:
   int posX = 0;
   int posY = 0;
 
   int velX = 0;
-public:
-  int getVelX() const;
-
-  int getVelY() const;
-
-private:
   int velY = 0;
 };
 
-#endif //GAMESERVER_DOT_H
+#endif  // CLIENT_VISUALS_DOT_H_
